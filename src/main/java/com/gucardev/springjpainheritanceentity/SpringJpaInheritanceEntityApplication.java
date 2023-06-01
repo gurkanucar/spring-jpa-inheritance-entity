d24cbca4733487d1957a1b271c8dc58143397d5a
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-02 01:18:37 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/springjpainheritanceentity/SpringJpaInheritanceEntityApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.springjpainheritanceentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaInheritanceEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaInheritanceEntityApplication.class, args);
+	}
+
+}
