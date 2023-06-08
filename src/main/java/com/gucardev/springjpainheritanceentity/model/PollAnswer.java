c92fa902c2ed89f68676729e85bd0c3d241fba33
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-08 23:12:35 +0300

    fix: Poll post relations fixed

--- src/main/java/com/gucardev/springjpainheritanceentity/model/PollAnswer.java
+++ src/main/java/com/gucardev/springjpainheritanceentity/model/PollAnswer.java
@@ -1,13 +1,24 @@
 package com.gucardev.springjpainheritanceentity.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-
 @Entity
-@Getter @Setter
+@Getter
+@Setter
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class PollAnswer {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,6 +26,7 @@ public class PollAnswer {
 
   @ManyToOne
   @JoinColumn(name = "poll_post_id")
+  @JsonBackReference
   private PollPost pollPost;
 
   private String answer;
@@ -23,7 +35,6 @@ public class PollAnswer {
   @JoinTable(
       name = "user_poll_answer",
       joinColumns = @JoinColumn(name = "poll_answer_id"),
-      inverseJoinColumns = @JoinColumn(name = "user_id")
-  )
+      inverseJoinColumns = @JoinColumn(name = "user_id"))
   private List<User> usersWhoVoted;
 }
--- src/main/java/com/gucardev/springjpainheritanceentity/model/PollPost.java
+++ src/main/java/com/gucardev/springjpainheritanceentity/model/PollPost.java
@@ -1,6 +1,10 @@
 package com.gucardev.springjpainheritanceentity.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
@@ -11,7 +15,9 @@ import lombok.Setter;
 @PrimaryKeyJoinColumn(name = "post_id")
 @Getter
 @Setter
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class PollPost extends Post {
-  @OneToMany(mappedBy = "pollPost")
+  @OneToMany(mappedBy = "pollPost", cascade = CascadeType.ALL)
+  @JsonManagedReference
   private List<PollAnswer> pollAnswers;
 }
