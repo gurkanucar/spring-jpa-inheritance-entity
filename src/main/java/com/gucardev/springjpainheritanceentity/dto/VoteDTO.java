1a660413c7a6e3115846c321602be5f09678d5e1
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-09 23:54:02 +0300

    feat: vote dto created

---
+++ src/main/java/com/gucardev/springjpainheritanceentity/dto/VoteDTO.java
@@ -0,0 +1,11 @@
+package com.gucardev.springjpainheritanceentity.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VoteDTO {
+  private Long userId;
+  private Long pollAnswerId;
+}
