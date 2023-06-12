aa753b18090e1fad12b51bb82106889f920199ba
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-12 21:04:46 +0300

    feat: voting endpoint added

--- src/main/java/com/gucardev/springjpainheritanceentity/controller/PostController.java
+++ src/main/java/com/gucardev/springjpainheritanceentity/controller/PostController.java
@@ -1,9 +1,12 @@
 package com.gucardev.springjpainheritanceentity.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.gucardev.springjpainheritanceentity.dto.PostDTO;
+import com.gucardev.springjpainheritanceentity.dto.VoteDTO;
 import com.gucardev.springjpainheritanceentity.enumeration.PostType;
 import com.gucardev.springjpainheritanceentity.model.Post;
 import com.gucardev.springjpainheritanceentity.service.PostService;
+import com.gucardev.springjpainheritanceentity.service.VotingService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,15 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/post")
 public class PostController {
   private final PostService postService;
+  private final VotingService votingService;
 
-  public PostController(PostService postService) {
+  public PostController(PostService postService, VotingService votingService) {
     this.postService = postService;
+    this.votingService = votingService;
   }
-
 
   @GetMapping
   public ResponseEntity<Object> get(@RequestParam(required = false) PostType postType) {
-    return ResponseEntity.ok(postService.getPostsByType(postType));
+    return ResponseEntity.ok(postService.getPostsByTypeDTO(postType));
   }
 
   @PutMapping
@@ -38,5 +42,11 @@ public class PostController {
   public ResponseEntity<Post> createPost(@RequestBody PostDTO postDto) {
     Post post = postService.createPost(postDto);
     return ResponseEntity.ok(post);
+  }
+
+  @PostMapping("/vote")
+  public ResponseEntity<?> vote(@RequestBody VoteDTO voteDto) {
+    votingService.vote(voteDto);
+    return ResponseEntity.ok().build();
   }
 }
