6593084f236ab7e87e431f02fe7fa401e51626d2
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-15 01:26:35 +0300

    feat: PostType enum updated with factory bean names

--- src/main/java/com/gucardev/springjpainheritanceentity/enumeration/PostType.java
+++ src/main/java/com/gucardev/springjpainheritanceentity/enumeration/PostType.java
@@ -1,15 +1,18 @@
 package com.gucardev.springjpainheritanceentity.enumeration;
 
 public enum PostType {
-  TEXT,
-  IMAGE,
-  VIDEO,
-  POLL;
+  TEXT(PostType.TEXT_FACTORY_NAME),
+  IMAGE(PostType.IMAGE_FACTORY_NAME),
+  VIDEO(PostType.VIDEO_FACTORY_NAME),
+  POLL(PostType.POLL_FACTORY_NAME);
 
-  public static PostType getEnumByString(String str) {
-    for (PostType type : PostType.values()) {
-      if (type.name().equalsIgnoreCase(str)) return type;
-    }
-    throw new IllegalArgumentException("Not found with text " + str);
-  }
+  public static final String TEXT_FACTORY_NAME = "textPostFactory";
+  public static final String IMAGE_FACTORY_NAME = "imagePostFactory";
+  public static final String VIDEO_FACTORY_NAME = "videoPostFactory";
+  public static final String POLL_FACTORY_NAME = "pollPostFactory";
+
+  private final String factoryBeanName;
+
+  PostType(String factoryBeanName) { this.factoryBeanName = factoryBeanName; }
+  public String factoryBeanName() { return factoryBeanName; }
 }
