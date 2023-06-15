433b3d9a71a32ec765b03144055b4e540cc23bcd
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-15 12:14:31 +0300

    feat: PostFactory and Producer updated

--- src/main/java/com/gucardev/springjpainheritanceentity/factory/post/create/PostFactory.java
+++ src/main/java/com/gucardev/springjpainheritanceentity/factory/post/create/PostFactory.java
@@ -1,11 +1,8 @@
 package com.gucardev.springjpainheritanceentity.factory.post.create;
 
 import com.gucardev.springjpainheritanceentity.dto.PostDTO;
-import com.gucardev.springjpainheritanceentity.enumeration.PostType;
 import com.gucardev.springjpainheritanceentity.model.Post;
 
 public interface PostFactory {
   Post createPost(PostDTO postDto);
-
-  PostType getPostType();
 }
--- src/main/java/com/gucardev/springjpainheritanceentity/factory/post/create/PostFactoryProducer.java
+++ src/main/java/com/gucardev/springjpainheritanceentity/factory/post/create/PostFactoryProducer.java
@@ -1,23 +1,19 @@
 package com.gucardev.springjpainheritanceentity.factory.post.create;
 
 import com.gucardev.springjpainheritanceentity.enumeration.PostType;
-import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PostFactoryProducer {
-  private final Map<PostType, PostFactory> factoryMap;
+  private final Map<String, PostFactory> factoryMap;
 
-  public PostFactoryProducer(List<PostFactory> factories) {
-    this.factoryMap =
-        factories.stream().collect(Collectors.toMap(PostFactory::getPostType, Function.identity()));
+  public PostFactoryProducer(Map<String, PostFactory> factoryMap) {
+    this.factoryMap = factoryMap;
   }
 
   public PostFactory getFactoryByType(PostType type) {
-    PostFactory factory = factoryMap.get(type);
+    PostFactory factory = factoryMap.get(type.factoryBeanName());
     if (factory == null) {
       throw new IllegalArgumentException("Invalid PostType provided");
     }
